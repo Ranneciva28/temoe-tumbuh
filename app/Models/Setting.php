041012b117fb3b6a0ef22a894dcd65f91ba646d4
Ranneciva28 <reddingtongaming28@gmail.com>
2026-09-05 17:49:01 +0700

    Encrypt secret settings at rest

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
+use Throwable;
 
 class Setting extends Model
 {
@@ -16,15 +18,30 @@ class Setting extends Model
     {
         return static::query()
             ->where('group', $group)
-            ->pluck('value', 'key')
+            ->get()
+            ->mapWithKeys(function (Setting $setting) {
+                $value = $setting->value;
+                if ($setting->is_secret && filled($value)) {
+                    try {
+                        $value = Crypt::decryptString($value);
+                    } catch (Throwable) {
+                        // Backward compatibility for any pre-encryption value.
+                    }
+                }
+                return [$setting->key => $value];
+            })
             ->all();
     }
 
     public static function put(string $group, string $key, mixed $value, bool $secret = false): void
     {
+        $storedValue = $secret && filled($value)
+            ? Crypt::encryptString((string) $value)
+            : $value;
+
         static::query()->updateOrCreate(
             ['key' => $key],
-            ['group' => $group, 'value' => $value, 'is_secret' => $secret]
+            ['group' => $group, 'value' => $storedValue, 'is_secret' => $secret]
         );
     }
 }
