6041a8f5d4b3fad5b7bc3d4780a05736d195b84b
Ranneciva28 <reddingtongaming28@gmail.com>
2026-09-05 17:50:17 +0700

    Add public privacy notice route

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
+Route::view('/privasi', 'privacy')->name('privacy');
 Route::get('/minat', [InterestController::class, 'create'])->name('interest.create');
 Route::post('/minat', [InterestController::class, 'store'])->name('interest.store');
 Route::get('/terima-kasih', [InterestController::class, 'thankYou'])->name('interest.thank-you');
