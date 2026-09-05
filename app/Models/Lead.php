4950154fc391067663a64623b37666e416c4ad46
Ranneciva28 <reddingtongaming28@gmail.com>
2026-09-05 17:35:13 +0700

    Cast dynamic lead fields

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -9,7 +9,7 @@ class Lead extends Model
     protected $fillable = [
         'parent_name','whatsapp','email','child_name','child_age','city','district',
         'preferred_location','preferred_schedule','preferred_start_date','budget_range',
-        'reservation_interest','status','notes','utm_source','utm_medium','utm_campaign',
+        'reservation_interest','status','notes','custom_fields','utm_source','utm_medium','utm_campaign',
         'utm_content','utm_term','fbclid','gclid','referrer','landing_page','ip_address',
         'user_agent','contacted_at','qualified_at','reserved_at',
     ];
@@ -19,6 +19,7 @@ class Lead extends Model
         return [
             'reservation_interest' => 'boolean',
             'preferred_start_date' => 'date',
+            'custom_fields' => 'array',
             'contacted_at' => 'datetime',
             'qualified_at' => 'datetime',
             'reserved_at' => 'datetime',
